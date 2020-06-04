5536b4b60a0183083df85296320e795e2facd48f
Shankar <psgganesh@gmail.com>
2020-06-04 10:55:34 +0800

    pushng changes

--- src/Connect.php
+++ src/Connect.php
@@ -32,5 +32,51 @@ class Connect
 			}
 
 		}
+    }
+    
+    public function connect_ldap() {
+
+        echo "reached function".PHP_EOL;
+        
+		$username = 'shankarg';
+		$password = 'sp63Zgzn';
+        
+        echo "user, pass initiated".PHP_EOL;
+
+		$options = [
+			'host' => 'ldap://corpldap2.sphnet.com.sg:389',
+			'ou' => 'sph', // such as "people" or "users"
+			'dc' => array('sphnet','com','sg'),
+		];
+        
+        echo "options initiated".PHP_EOL;
+
+		$dc_string = implode(".",$options['dc']);
+		$connection = ldap_connect($options['host']);
+		ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
+        
+
+        echo "tried ldap_connect".PHP_EOL;
+
+        echo $connection;
+
+		if($connection)
+		{
+
+            echo "trying to bind".PHP_EOL;
+            
+		    $bind = @ldap_bind($connection, "{$username}@{$dc_string}", $password) or die("Could not bind to LDAP");
+            
+            echo "after bind".PHP_EOL;
+
+            if($bind){
+			    echo "logged in";
+			} else {
+				echo "not logged in";	
+			} 
+		}			 
+
+		//return !$this->errorCode;		
 	}
 }
